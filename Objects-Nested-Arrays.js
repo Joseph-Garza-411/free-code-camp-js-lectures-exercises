fdb4a2747e2beff4788538ca829e6c4edb643ad6
Joseph Garza <teacherjosephg@gmail.com>
2021-10-23 13:57:18 -0500

    Add loop 4 & 5 to arr

--- Objects-Nested-Arrays.js
+++ Objects-Nested-Arrays.js
@@ -1,6 +1,25 @@
-for(let i = 0; i < 5; i++){
-    console.log("First Level Loop"); 
-for(let j = 0; j <= 1; j++){
-    console.log("--Second Level Loop"); 
-}
-}
+// for(let i = 0; i < 5; i++){
+//     console.log("First Level Loop"); 
+// for(let j = 0; j <= 1; j++){
+//     console.log("--Second Level Loop"); 
+// }
+// 
+
+
+
+let arr = [
+    [1,2], 
+    [3,4], 
+    [5,6],
+    ['loop4', 4], 
+    ['loop5', 5],  
+]
+
+// console.log(arr[0].length+ ` , `+ arr[1].length+ ` , ` + arr[2].length);  
+// console.log(arr); 
+// console.log(arr.length); 
+// console.log[3]; 
+// console.log(arr.length); 
+console.log(arr[3]); 
+console.log(arr[4]);
+console.log(arr[5]);  

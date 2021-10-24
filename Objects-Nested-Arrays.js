8f70a4e402062679f780bcfce418b642f8f6cefb
Joseph Garza <teacherjosephg@gmail.com>
2021-10-23 23:50:39 -0500

    Add final comments before going to bed.

--- Objects-Nested-Arrays.js
+++ Objects-Nested-Arrays.js
@@ -7,19 +7,41 @@
 
 
 
-let arr = [
-    [1,2], 
-    [3,4], 
-    [5,6],
-    ['loop4', 4], 
-    ['loop5', 5],  
-]
+// let arr = [
+//     [1,2], 
+//     [3,4], 
+//     [5,6],
+//     ['loop4', 4], 
+//     ['loop5', 5],  
+// ]
 
 // console.log(arr[0].length+ ` , `+ arr[1].length+ ` , ` + arr[2].length);  
 // console.log(arr); 
 // console.log(arr.length); 
 // console.log[3]; 
 // console.log(arr.length); 
-console.log(arr[3]); 
-console.log(arr[4]);
-console.log(arr[5]);  
+// console.log(arr[3]); 
+// console.log(arr[4]);
+// console.log(arr[5]);  
+
+
+var myNoun = " dog";
+var myAdjective = " big";
+var myVerb = " ran";
+var myAdverb = " quickly";
+
+// Only change code below this line
+    myAdjective += myNoun;
+    myVerb +=  myAdverb; 
+    myAdjective += myVerb; 
+    var wordBlanks = "That" + myAdjective + "."; // Change this line
+// Only change code above this line
+
+// console.log(myAdjective); 
+// console.log(myVerb); 
+console.log(wordBlanks); 
+
+
+
+
+

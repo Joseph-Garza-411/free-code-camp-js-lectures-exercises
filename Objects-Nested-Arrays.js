3d18567574a8a2453b6eff367b2d840dd8cf9cb5
Joseph Garza <teacherjosephg@gmail.com>
2021-10-26 22:21:50 -0500

    Add comments to break for the night.

--- Objects-Nested-Arrays.js
+++ Objects-Nested-Arrays.js
@@ -42,24 +42,52 @@
 // console.log(wordBlanks); 
 
 
-function nextInLine(arr, item) {
-    // Only change code below this line
-    let localM = arr.push(item); 
+// function nextInLine(arr, item) {
+//     // Only change code below this line
+//     let localM = arr.push(item); 
     
-    return arr.shift();
-    // Only change code above this line
+//     return arr.shift();
+//     // Only change code above this line
     
   
+//   }
+  
+//   // Setup
+//   var testArr = [1,2,3,4,5];
+  
+//   // Display code
+//   console.log("Before: " + JSON.stringify(testArr));
+//   console.log(nextInLine(testArr, 6));
+//   console.log("After: " + JSON.stringify(testArr));
+// //   console.log(arr.length); 
+
+// function trueOrFalse(wasThatTrue) {
+//   // Only change code below this line
+//   if(wasThatTrue){
+//     return "Yes, that was true."; 
+//   }
+//     return "No, that was false."; 
+//   // Only change code above this line
+// }
+
+// let answer = trueOrFalse(true); 
+// console.log(answer); 
+// trueOrFalse(false); 
+
+function orderMyLogic(val) {
+ 
+  if (val < 5) {
+    return "Less than five";
+    console.log("Less than five"); 
+  } else if (val < 10) {
+    return "Less than ten";
+  } else {
+    return "Greater than or equal to 10";
   }
-  
-  // Setup
-  var testArr = [1,2,3,4,5];
-  
-  // Display code
-  console.log("Before: " + JSON.stringify(testArr));
-  console.log(nextInLine(testArr, 6));
-  console.log("After: " + JSON.stringify(testArr));
-//   console.log(arr.length); 
+}
+
+orderMyLogic(4);
+
 
 
 
--- multiplyAll_function_nestedArrays.js
+++ multiplyAll_function_nestedArrays.js
@@ -18,15 +18,41 @@ const { arrayBuffer } = require("stream/consumers");
 
 // multiplyAll([[1,2], [3,4], [5,6,7]]); 
 
-function add(){
-    let sum = 0; 
-    for(let i = 0; i < arguments.length; i++){
-        sum += arguments[i]; 
-    }
-    return sum; 
+// function add(){
+//     let sum = 0; 
+//     for(let i = 0; i < arguments.length; i++){
+//         sum += arguments[i]; 
+//     }
+//     return sum; 
+// }
+
+// console.log(add(1,2)); 
+
+
+
+// function orderMyLogic(val){
+//     if(val < 5){
+//         return "Less than five"; 
+//     }else if (val < 10){
+//         return "Less than ten";
+//     }return "Greater than or equal to 10"; 
+// }
+// console.log(orderMyLogic(5)); 
+
+
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+
+
+  return par / strokes;
+  // Only change code above this line
 }
 
-console.log(add(1,2)); 
+console.log(golfScore(5, 4));
+
+
 
 
 

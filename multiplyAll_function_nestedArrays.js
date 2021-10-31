f4ca9e375cce103de7135fe01ed3d8f1953feeb8
Joseph Garza <teacherjosephg@gmail.com>
2021-10-30 21:45:16 -0500

    Add comments.

--- multiplyAll_function_nestedArrays.js
+++ multiplyAll_function_nestedArrays.js
@@ -295,47 +295,161 @@ Add a new album to the myMusic array. Add artist and title strings,
 release_year number and a formats array of strings. 
 */ 
 
-const myMusic = [
-  {
-    "artist": "Billy Joel",
-    "title": "Piano Man",
-    "release_year": 1973,
-    "formats": [
-      "CD",
-      "8T",
-      "LP"
-    ],
-    "gold": true
-  }
-];
+// const myMusic = [
+//   {
+//     "artist": "Billy Joel",
+//     "title": "Piano Man",
+//     "release_year": 1973,
+//     "formats": [
+//       "CD",
+//       "8T",
+//       "LP"
+//     ],
+//     "gold": true
+//   }
+// ];
 
-myMusic.push(
-  {
-    "artist": "Wildman",
-    "title" : "The Incredible Song", 
-    "release_year" : 1999, 
-    "formats" : [
-      "CD", 
-      "LP"
-    ], 
-    "gold": true
-  }
-); 
+// myMusic.push(
+//   {
+//     "artist": "Wildman",
+//     "title" : "The Incredible Song", 
+//     "release_year" : 1999, 
+//     "formats" : [
+//       "CD", 
+//       "LP"
+//     ], 
+//     "gold": true
+//   }
+// ); 
 
 
  
 
 //  console.log(myMusic); 
 //  console.log(myMusic.length); 
-console.log(myMusic[1]);
+// console.log(myMusic[1]);
 
 
 
+// const myStorage = {
+//   "car": {
+//     "inside": {
+//       "glove box": "maps",
+//       "passenger seat": "crumbs"
+//      },
+//     "outside": {
+//       "trunk": "jack"
+//     }
+//   }
+// };
 
 
+// const myPlants = [
+//   {
+//     type: "flowers",
+//     list: [
+//       "rose",
+//       "tulip",
+//       "dandelion"
+//     ]
+//   },
+//   {
+//     type: "trees",
+//     list: [
+//       " fir",
+//       " pine",
+//       " birch"
+//     ]
+//   }
+// ];
+
+// // const secondTree = "";
+// let element0 = myPlants[1].list[0];
+// let element1 = myPlants[1].list[1];
+// let element2 = myPlants[1].list[2];  
+// console.log(element0 + ',' + element1 + ',' + element2); 
+
+// const gloveBoxContents = myStorage.car.inside["glove box"];  
+// console.log(myStorage.car); 
+
+// const recordCollection = {
+//   2548: {
+//     albumTitle: 'Slippery When Wet',
+//     artist: 'Bon Jovi',
+//     tracks: ['Let It Rock', 'You Give Love a Bad Name']
+//   },
+//   2468: {
+//     albumTitle: '1999',
+//     artist: 'Prince',
+//     tracks: ['1999', 'Little Red Corvette']
+//   },
+//   1245: {
+//     artist: 'Robert Palmer',
+//     tracks: []
+//   },
+//   5439: {
+//     albumTitle: 'ABBA Gold'
+//   }
+// };
+
+// // Only change code below this line
+
+// // console.log(recordCollection); 
+// function updateRecords(records, id, prop, value) {
+// if (prop !== 'tracks' && value !== ""){
+//   records[id][prop]= value; 
+// } else if (prop == 'tracks' && records[id].hasOwnProperty('tracks') === false){
+//   records[id][prop] = [value]; 
+// } else if (prop == 'tracks' && value !== ""){
+//   records[id][prop].push(value); 
+// } else if (value == ""){
+//   delete records[id][prop]; 
+// }
+//   return records;
+// }
+
+// updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+// console.log(recordCollection); 
 
 
+// Setup
+// const myArray = [];
 
+// Only change code below this line
+
+// let i = 5; 
+// while (i >= 0){
+//   myArray.push(i);
+//   i--; 
+//   }
+
+// console.log(myArray);
+
+// Setup
+// const myArray = [];
+
+// Only change code below this line
+// for (let i = 0; i <= 5; i++){
+//   myArray.push(i); 
+// }
+// console.log(myArray); 
+const myArr = [2, 3, 4, 5, 6];
+  let total = 0; 
+  for (let i = 0; i < myArr.length; i++){
+    total += myArr[i]; 
+    
+    console.log(total); 
+    
+}
+
+// const arr = [10, 9, 8, 7, 6];
+
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+// }
+
+// console.log(arr.length);
 
 
 
